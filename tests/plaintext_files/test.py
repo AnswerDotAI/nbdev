c7457b0991a47369d618e6419675571d85e4b1be
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-02-22 21:23:22 +0000

    test: Test for the `fmt` argument in NBProcessor and nb_export

---
+++ tests/plaintext_files/test.py
@@ -0,0 +1,15 @@
+# %% [markdown]
+# This is a
+# plain-text notebook.
+
+# %% [markdown]
+# The format of the notebook is `percent`.
+
+# %%
+#|default_exp pct2.test
+
+# %%
+#|export
+# This is a code cell
+def foo():
+    return "bar"
