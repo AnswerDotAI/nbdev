b02bac94cdfeff149cee5da8c09fe35e9d70925b
Jeremy Howard <github@jhoward.fastmail.fm>
2026-07-31 15:02:58 +1000

    bump

--- nbdev/__init__.py
+++ nbdev/__init__.py
@@ -5,7 +5,7 @@ Modules:
 - `nbdev.extract_attachments`: A preprocessor that extracts all of the attachments from the notebook file. The extracted attachments are returned in the 'resources' dictionary.
 - `nbdev.skill`: Author clear, executable nbdev notebooks where code, prose, examples, outputs, and tests form one coherent narrative."""
 
-__version__ = "3.3.3"
+__version__ = "3.3.4"
 
 from .doclinks import nbdev_export
 from .showdoc import show_doc
